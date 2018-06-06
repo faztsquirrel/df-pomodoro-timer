cc15271fb8fbf8955ee803e27eb0c914dcfcaa69
faztsquirrel <redisconnect@gmail.com>
2018-06-06 04:33:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Distraction-Free Pomodoro Timer
+
+Countless Pomodoro Timers exist, but all I wanted was a simple timer with no 
+ads, no fancy animations and no online services. So I created this little
+one-page website with the following features:
+
+* Timing using the Pomodoro technique
+* Text based timer, no animations
+* Editable choice of task
+* Log of completed Pomodoros
+* Persistent changes, through local storage
+* Works perfectly offline using caching
+* Can be installed as WebApp on mobile devices
+
+## Usage
+
+Simply visithttps://faztsquirrel.github.io/df-pomodoro-timer/
+
+Or clone this repository and open `index.html`!
---
+++ service_worker.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'pomodoro-cache';
+var urlsToCache = [
+  '/android-chrome-192x192.png',
+  'android-chrome-512x512.png',
+  'apple-touch-icon.png',
+  'browserconfig.xml',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'favicon.ico',
+  'index.html',
+  'mstile-150x150.png',
+  'pomodoro.webmanifest',
+  'safari-pinned-tab.svg',
+  'service_worker.js',
+  'site.webmanifest',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
